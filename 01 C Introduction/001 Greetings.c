a2e319a0a992a5900dc9e51cf4b8e729a5ad9a08
prasanth-vaindam <107233850+prasanth-vaindam@users.noreply.github.com>
2024-06-25 08:24:31 +0530

    first commit

---
+++ 01 C Introduction/001 Greetings.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("Namaste Bharat!");
+    return 0;
+}
---
+++ 06 C Pointers/1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int var = 100, a = 10, b = 20;
+    printf("a:%d b:%d\n",a,b);
+    printf("address of var is :%p\n",&var);
+    return 0;
+}
