3babc70c171263fafd2afc40e2da17126198b5df
prasanth-vaindam <107233850+prasanth-vaindam@users.noreply.github.com>
2024-07-11 18:14:54 +0530

    Day 14 for batch 2

--- 02 C Fundas/Day 04 - Operators/01 Arithmetic Operators.c
+++ 02 C Fundas/Day 04 - Operators/01 Arithmetic Operators.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 int main()
 {
-    int a = 9,b = 4, c;
-    
+    int a = 9,b = 4;
+    float c;
     c = a+b;
-    printf("a+b = %d \n",c);
+    printf("a+b = %d \n",(int)c); // 10
     c = a-b;
-    printf("a-b = %d \n",c);
+    printf("a-b = %d \n",(int)c); // 5
     c = a*b;
-    printf("a*b = %d \n",c);
-    c = a/b;
-    printf("a/b = %d \n",c);
+    printf("a*b = %d \n", (int) c); // 36
+    c = (float)a/b;
+    printf("a/b = %f \n",c); // 2.25
     c = a%b;
-    printf("Remainder when %d divided by %d = %d \n", a,b,c);
+    printf("Remainder when %d divided by %d = %d \n", a,b,c); // 1
     
     return 0;
 }
